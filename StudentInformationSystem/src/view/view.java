77079c737113034b80bc87b743ec2e8f7ec109ad
Rahul <rahulyadav190582@gmail.com>
2022-07-13 11:39:35 +0530

    first commit

---
+++ StudentInformationSystem/src/view/view.java
@@ -0,0 +1,13 @@
+package view;
+
+import model.Student;
+
+public class view {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		Student student=new Student();
+	}
+
+}
